fd5aefe46087c9262489164e1295872e33f2007e
wangqiang <494284743@qq.com>
2016-11-13 20:15:43 +0800

    question

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from 'react';  //var React = require("React")
+import ReactDOM from 'react-dom';
+import TodoMVC from './todoMVC2/index.js';
+// import './index.css';//require("./index.css")
+
+ReactDOM.render(
+  <TodoMVC />,
+  document.getElementById('root')
+);
